510bc5e5b48a99f5a10ab8abcad8278dc084badb
jiteshjha <jiteshjha96@gmail.com>
2016-10-12 20:28:34 +0530

    Frames are now compressed in JPEG

--- ScreenCastFeed.py
+++ ScreenCastFeed.py
@@ -1,6 +1,9 @@
 import cv2
 import numpy as np
 import pyscreenshot as ImageGrab
+import io
+import sys
+from PIL import Image
 
 class ScreenCastFeed:
     def __init__(self, name="sc"):
@@ -9,12 +12,19 @@ class ScreenCastFeed:
     def get_frame(self):
         img = ImageGrab.grab()
     	img_np = np.array(img)
-        print img_np.size
-    	return img_np
 
-    def set_frame(self, img_np):
-    	frame = cv2.cvtColor(img_np, cv2.COLOR_BGR2GRAY)
-    	cv2.imshow("test", frame)
+        frame = cv2.cvtColor(img_np, cv2.COLOR_RGB2BGR)
+        pil_im = Image.fromarray(frame)
+        b = io.BytesIO()
+        pil_im.save(b, 'jpeg')
+        im_bytes = b.getvalue()
+        return im_bytes
+
+    def set_frame(self, frame_bytes):
+    	pil_bytes = io.BytesIO(frame_bytes)
+        pil_image = Image.open(pil_bytes)
+        cv_image = cv2.cvtColor(np.array(pil_image), cv2.COLOR_RGB2BGR)
+        cv2.imshow("test", cv_image)
     	cv2.waitKey(0)
 
 if __name__=="__main__":
